1cc2936d44dce067df5284538a81f4b7c5c52292
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-03 22:18:35 +0530

    Downloaded dummy data

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="container">
+      <h3>Filter Table Data</h3>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <App />
+  ,
+)
